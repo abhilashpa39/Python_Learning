c374e50be22402a6aca1561f73082f8a873e8db4
Abhilash Pulikkal Asokan <ap22aco@herts.ac.uk>
2023-03-03 13:26:18 +0000

    Swap two variables without using a temporary variable

---
+++ swap_two_variables_without_temp_variable.py
@@ -0,0 +1,7 @@
+# Swap Two Variables Without Using Temporary Variable
+# In Python, there is a simple construct to swap variables
+x = 5
+y = 10
+x, y = y, x
+print('Value of X =', x)
+print('Value of Y =', y)
