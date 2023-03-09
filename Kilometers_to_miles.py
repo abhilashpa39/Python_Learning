67f7becec2525897b27b25a404deae243c62f411
Abhilash Pulikkal Asokan <ap22aco@herts.ac.uk>
2023-03-09 02:22:27 +0000

    Kilometers to miles conversion

---
+++ Kilometers_to_miles.py
@@ -0,0 +1,10 @@
+# Kilometers to Miles
+# Taking Kilometers input from user
+kilometer = float(input("Enter the kilometers: "))
+
+#Conversion factor
+factor = 0.621371
+
+# Calculate Miles
+miles = kilometer * factor
+print("%0.2f Kilometers is equal to %0.2f Miles" %(kilometer,miles))
